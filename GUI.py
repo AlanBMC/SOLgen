fac8961ba818972336d48ff121147ca2251392b3
AlanBMC <alanbmc00@gmail.com>
2024-09-10 18:53:52 -0400

    feat: criando tela principal

--- GUI.py
+++ GUI.py
@@ -33,5 +33,84 @@ def main(page: ft.Page):
             itens_lidos.remove(produto)
             atualizar_cards()
 
+    def adicionar_card(produto):
+        card = ft.Card(
+            content=ft.Container(
+                content=ft.Column(
+                    [
+                        ft.Text(produto['nome'], max_lines=1, overflow="ellipsis",
+                                bgcolor='#FFFFFF', color='#158711'),
+                        ft.Text(produto['preco']),
+                        ft.Text(produto['codigo']),
+                        ft.Row(
+                            [
+                                ft.ElevatedButton(
+                                    "Excluir", on_click=excluir_produto, data=produto)
+                            ],
+                            alignment=ft.MainAxisAlignment.END
+                        )
+                    ]
+                ),
+                width=200, padding=10, bgcolor="#E9D345", border_radius=8
+            ),
+        )
+        cards_list.controls.append(card)
+
+    def atualizar_cards():
+        cards_list.controls.clear()
+        for produto in itens_lidos:
+            adicionar_card(produto)
+        cards_list.update()
+
+    def pesquisa_codigo(e):
+        codigo = e.control.value.strip()
+
+        produto = encontrar_produto_por_codigo(codigo)
+
+        if produto and produto not in itens_lidos:
+            itens_lidos.append(produto)
+            adicionar_card(produto)
+            input_codigo.value = ''
+            input_codigo.focus()
+        page.update()
+        input_codigo.autofocus = True
+
+    input_codigo = ft.TextField(
+        label="Escaneie o código do produto",
+        bgcolor='#525252', color="#fafa3c", on_submit=pesquisa_codigo, autofocus=True
+    )
+
+    def prepara_dados_para_impressao(e):
+        quantidade = input_quantidade_por_produto.value
+        eti = tipo_etiqueta.value
+        if eti is None:
+            print("Tipo de etiqueta não selecionado.")
+            page.add(ft.Text('Por favor, selecione um tipo de etiqueta.'))
+            page.update()
+            return
+        if envia_para_impressora(itens_lidos, int(eti), quantidade):
+            page.add(ft.Text('Impressão acontecendo...'))
+            page.update()
+
+    input_quantidade_por_produto = ft.Dropdown(
+        width=100, options=[ft.dropdown.Option(1), ft.dropdown.Option(2), ft.dropdown.Option(3), ft.dropdown.Option(4)]
+    )
+
+    tipo_etiqueta = ft.RadioGroup(
+        content=ft.Column([
+            ft.Radio(value=1, label="Etiqueta Amarela"),
+            ft.Radio(value=2, label="Etiqueta Branca")
+        ])
+    )
+
+    enviar_impressao = ft.ElevatedButton(
+        'Enviar impressão', on_click=prepara_dados_para_impressao)
+    homePage = ft.ElevatedButton("HomePage", on_click=homepage)
+    lista_homePage = [ft.Row([ft.ElevatedButton(
+        "Impressora", on_click=impressoras), homePage])]
+    lista_impressora =[homePage,input_codigo, tipo_etiqueta,
+                        input_quantidade_por_produto, enviar_impressao, cards_list]
+    page.add(*lista_homePage)
+
 
 ft.app(main)
