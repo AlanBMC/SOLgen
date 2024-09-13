9bc9acfbc851e26cf593e96210abb7887e86f58c
AlanBMC <alanbmc00@gmail.com>
2024-09-13 15:13:29 -0400

    feat: criando estrutura de fluxo para o pyautogui

---
+++ src/componentes/produtos/pyautogui_cadastra_produtos.py
@@ -0,0 +1,31 @@
+import pyautogui
+
+
+def digita_texto(texto):
+    pass
+
+def cola_texto(texto):
+    pass
+def clica_ok():
+    pass
+
+def clica_fiscal():
+    pass
+
+def clica_cfop(cfop):
+    pass
+
+def clica_ncm(ncm):
+    pass
+
+def clica_nome(nome):
+    pass
+
+def clica_preco(preco):
+    pass
+
+def clica_preco_compra(preco_un):
+    pass
+
+def clica_salvar():
+    pass
