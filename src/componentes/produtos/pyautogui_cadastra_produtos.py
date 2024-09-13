f2c0113da815703408f0631d50f18568424b17fc
AlanBMC <alanbmc00@gmail.com>
2024-09-13 15:18:43 -0400

    feat: estabelencendo quem vai ser o texto digitado e ou escrito

--- src/componentes/produtos/pyautogui_cadastra_produtos.py
+++ src/componentes/produtos/pyautogui_cadastra_produtos.py
@@ -1,11 +1,23 @@
 import pyautogui
-
+import pyperclip
+import time
 
 def digita_texto(texto):
-    pass
+    time.sleep(0.5)
+    pyautogui.write(texto, interval=0.05) 
+    
 
 def cola_texto(texto):
-    pass
+    """
+    Função para colar texto usando pyperclip e pyautogui.
+    
+    :param texto: String a ser colada.
+    """
+    pyperclip.copy(texto)
+    time.sleep(0.5)
+    pyautogui.hotkey('ctrl', 'v')
+    
+#pyautogui.typewrite(texto) # Digita o texto na tela
 def clica_ok():
     pass
 
@@ -13,18 +25,25 @@ def clica_fiscal():
     pass
 
 def clica_cfop(cfop):
+
+    digita_texto(cfop)
     pass
 
 def clica_ncm(ncm):
+    digita_texto(ncm)
     pass
 
 def clica_nome(nome):
+    cola_texto(nome)
     pass
 
 def clica_preco(preco):
+    cola_texto(preco)
     pass
 
 def clica_preco_compra(preco_un):
+    
+    cola_texto(preco_un)
     pass
 
 def clica_salvar():
