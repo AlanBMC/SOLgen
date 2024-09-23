a983dda712bc2dc65d507897f6acf26814cf402b
AlanBMC <alanbmc00@gmail.com>
2024-09-23 09:44:10 -0400

    feat: criando arquivos

---
+++ gerenciador/solge/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin # type: ignore
+from django.urls import path, include # type: ignore
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
