b203596b0c4658a5287f4c336d2d46f6d9addd43
AlanBMC <alanbmc00@gmail.com>
2024-09-23 14:00:23 -0400

    feat: criando cookie

--- gerenciador/solge/static/js/tabela.js
+++ gerenciador/solge/static/js/tabela.js
@@ -59,4 +59,21 @@ function toggleMenu() {
                 }
             });
         };
+
+
+        function getCookie(name) {
+            let cookieValue = null;
+            if (document.cookie && document.cookie !== '') {
+                const cookies = document.cookie.split(';');
+                for (let i = 0; i < cookies.length; i++) {
+                    const cookie = cookies[i].trim();
+                    // Verifica se esse cookie começa com o nome fornecido
+                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
         
